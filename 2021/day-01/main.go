b251a44fdd8e0ccfbc89822600caee01b2e2db85
Mawdac <ereilly@lookingglasscyber.com>
2022-11-12 17:14:02 -0500

    feat(2021-day-01): add part 2 solution and add comments

--- 2021/day-01/main.go
+++ 2021/day-01/main.go
@@ -14,39 +14,69 @@ func main() {
 	fmt.Printf("Total increases on sweep depth: %d\n", measureSweeps(input))
 }
 
+// count the amount of increases between iterations for a given []int
 func measureSweeps(sweeps []int) int {
-	var increases int
+	var positiveCount int
+	var currRange, prevRange []int
 
-	for i, v := range sweeps {
+	// for each sweep
+	for i := range sweeps {
+		// if it's the first time, skip
 		if i == 0 {
-			fmt.Printf("Skipping first iteration %d\n", v)
+			currRange = sweeps[i : i+3]
 			continue
-		} else if v > sweeps[i-1] {
-			increases++
 		}
-		fmt.Printf("Increases: %d - Previous: %d - Current %d\n", increases, sweeps[i-1], v)
+
+		// if there's not enough elements left, we're done
+		if i+3 > len(sweeps) {
+			break
+		}
+
+		// set the previous range, then update the current one
+		prevRange = currRange
+		currRange = sweeps[i : i+3]
+
+		// if the sum of the current is more than the sum of the previous
+		if sum(currRange) > sum(prevRange) {
+			positiveCount++
+		}
 	}
 
-	return increases
+	return positiveCount
 }
 
+// calculate the sum of a slice of integers
+func sum(sweeps []int) int {
+	total := 0
+	for _, v := range sweeps {
+		total += v
+	}
+	return total
+}
+
+// reads the input from a file and converts it to an array of integers
 func readInput(file string) []int {
 	var values []int
 
+	// open the file
 	f, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// remember to close it
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
 
+	// for each line
 	for scanner.Scan() {
+		// conver it to an int
 		x, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
+		// append it to the return array
 		values = append(values, x)
 	}
 

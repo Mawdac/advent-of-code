fc08274789f34f33f0e5055e94748c7a739b9d2d
Mawdac <edwardreillycomputers@gmail.com>
2023-12-02 15:32:18 -0500

    feat: 2023 day 2

---
+++ 2023/day-02/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type set struct {
+	red   int
+	green int
+	blue  int
+}
+type game struct {
+	sets []set
+}
+
+func main() {
+	var games []game
+	if len(os.Args) > 1 {
+		games = readGames(os.Args[1])
+		fmt.Printf("Read in %v games", len(games))
+	}
+
+	if len(os.Args) > 2 {
+		for i, v := range games {
+			fmt.Println("Game: ", i+1)
+			fmt.Println("Sets:", v)
+		}
+		if os.Args[2] == "part1" || os.Args[2] == "all" {
+			p1 := part1(games)
+			fmt.Println("~ Part 1 Solution ~")
+			fmt.Printf("The solution for part 1 is %v\n", p1)
+		}
+		if os.Args[2] == "part2" || os.Args[2] == "all" {
+			p2 := part2(games)
+			fmt.Println("\n~ Part 2 Solution ~")
+			fmt.Printf("The solution for part 2 is %v\n", p2)
+		}
+	} else {
+		fmt.Println("Missing command flags")
+		fmt.Println("Try this: 'go run main.go input.txt all/part1/part2'")
+	}
+}
+
+func part1(games []game) int {
+	total := 0
+	for i, g := range games {
+		possible := true
+		for _, s := range g.sets {
+			if s.red > 12 || s.green > 13 || s.blue > 14 {
+				possible = false
+			}
+		}
+		if possible {
+			total += i + 1
+		}
+	}
+	return total
+}
+
+func part2(games []game) int {
+	total := 0
+	for _, g := range games {
+		var redMax, greenMax, blueMax int
+		for _, s := range g.sets {
+			if s.red > redMax {
+				redMax = s.red
+			}
+			if s.green > greenMax {
+				greenMax = s.green
+			}
+			if s.blue > blueMax {
+				blueMax = s.blue
+			}
+		}
+		total += (redMax * greenMax * blueMax)
+	}
+	return total
+}
+
+func readGames(file string) []game {
+	returnGames := []game{}
+	f, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatalf("Could not read %v: %v", file, err)
+	}
+
+	trimmedFile := strings.TrimSuffix(string(f), "\n")
+	lines := strings.Split(trimmedFile, "\n")
+
+	for _, v := range lines {
+		returnGames = append(returnGames, parseGame(trim(v)))
+	}
+
+	return returnGames
+}
+
+func parseGame(line string) game {
+	returnGame := game{}
+	splitGameFromIndex := strings.Split(line, ":")
+	splitSets := strings.Split(trim(splitGameFromIndex[1]), ";")
+	for _, v := range splitSets {
+		newSet := set{}
+		splitCubes := strings.Split(trim(v), ",")
+		for _, v2 := range splitCubes {
+			countAndColor := strings.Split(trim(v2), " ")
+			addColorCount(countAndColor, &newSet)
+		}
+		returnGame.sets = append(returnGame.sets, newSet)
+	}
+	return returnGame
+}
+
+func addColorCount(countAndColor []string, s *set) {
+	count, _ := strconv.Atoi(countAndColor[0])
+	switch countAndColor[1] {
+	case "red":
+		s.red += count
+	case "green":
+		s.green += count
+	case "blue":
+		s.blue += count
+	}
+}
+
+func trim(s string) string {
+	return strings.TrimSpace(s)
+}
---
+++ 2023/day-02/main_test.go
@@ -0,0 +1,304 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_part1(t *testing.T) {
+	type args struct {
+		games []game
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test data",
+			args: args{
+				[]game{
+					{
+						sets: []set{
+							{
+								red:  4,
+								blue: 3,
+							},
+							{
+								red:   1,
+								green: 2,
+								blue:  6,
+							},
+							{
+								green: 2,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								green: 2,
+								blue:  1,
+							},
+							{
+								red:   1,
+								green: 3,
+								blue:  4,
+							},
+							{
+								green: 1,
+								blue:  1,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								red:   20,
+								green: 8,
+								blue:  6,
+							},
+							{
+								red:   3,
+								green: 13,
+								blue:  5,
+							},
+							{
+								green: 5,
+								red:   1,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								red:   3,
+								green: 1,
+								blue:  6,
+							},
+							{
+								red:   6,
+								green: 3,
+							},
+							{
+								green: 3,
+								red:   14,
+								blue:  15,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								red:   6,
+								green: 3,
+								blue:  1,
+							},
+							{
+								red:   1,
+								blue:  2,
+								green: 2,
+							},
+						},
+					},
+				},
+			},
+			want: 8,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.args.games); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2(t *testing.T) {
+	type args struct {
+		games []game
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test data",
+			args: args{
+				[]game{
+					{
+						sets: []set{
+							{
+								red:  4,
+								blue: 3,
+							},
+							{
+								red:   1,
+								green: 2,
+								blue:  6,
+							},
+							{
+								green: 2,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								green: 2,
+								blue:  1,
+							},
+							{
+								red:   1,
+								green: 3,
+								blue:  4,
+							},
+							{
+								green: 1,
+								blue:  1,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								red:   20,
+								green: 8,
+								blue:  6,
+							},
+							{
+								red:   3,
+								green: 13,
+								blue:  5,
+							},
+							{
+								green: 5,
+								red:   1,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								red:   3,
+								green: 1,
+								blue:  6,
+							},
+							{
+								red:   6,
+								green: 3,
+							},
+							{
+								green: 3,
+								red:   14,
+								blue:  15,
+							},
+						},
+					},
+					{
+						sets: []set{
+							{
+								red:   6,
+								green: 3,
+								blue:  1,
+							},
+							{
+								red:   1,
+								blue:  2,
+								green: 2,
+							},
+						},
+					},
+				},
+			},
+			want: 2286,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.args.games); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_readGames(t *testing.T) {
+	type args struct {
+		file string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []game
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := readGames(tt.args.file); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("readGames() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseGame(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want game
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseGame(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseGame() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_addColorCount(t *testing.T) {
+	type args struct {
+		countAndColor []string
+		s             *set
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			addColorCount(tt.args.countAndColor, tt.args.s)
+		})
+	}
+}
+
+func Test_trim(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := trim(tt.args.s); got != tt.want {
+				t.Errorf("trim() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
